7df3be9e55929e04849d671240a2875a2225d3b0
Your Name <you@example.com>
2022-08-12 12:34:22 +0300

    fix: html prettier and correction of code

--- 02/ht/example/formScript.js
+++ 02/ht/example/formScript.js
@@ -1,49 +1,16 @@
 const formContent = document.querySelector("#formContent");
 
-let content = `
-<br><form id="form">
-<label for="name">Name:</label><br>
-<input type="text" id="name" name="name"><br><br>
-<label for="city">City:</label><br>
-<select name="city" id="city">
-    <option value="Minsk">Minsk</option>
-    <option value="Grodno">Grodno</option>
-    <option value="Brest">Brest</option>
-    <option value="Gomel">Gomel</option>
-</select><br><br>
-<label for="commented">Commented:</label><br>
-<textarea id="commented" name="commented"></textarea><br><br>
-<label for="male">male:</label><br>
-<input type="radio" id="woman" name="male" value="woman" checked>
-<label for="woman">woman</label><br>
-<input type="radio" id="man" name="male" value="man">
-<label for="man">man</label><br><br>
-<button id="btnsbm">submit</button>
-</form>
-<div id="titleform"></div>`;
-
-formContent.innerHTML = content;
-
-const form = document.querySelector("#form");
-
-let textContent = "";
-
-const btnsbm = document.querySelector("#btnsbm");
-btnsbm.addEventListener("click", function (e) {
+formContent.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (
-    document.getElementById("name").value === "" ||
-    document.getElementById("name").value === ""
-  ) {
-    return alert("Заполните все поля!");
-  }
-  titleform = document.querySelector("#titleform");
+  const titleform = document.querySelector("#titleform");
   const formData = new FormData(form);
   const values = Object.fromEntries(formData);
-  textContent =
-    `<p> Зовут ${values.name}-${values.male}, я из ${values.city}.<br> Commented: '${values.commented}'</p>` +
-    textContent;
-  titleform.innerHTML = textContent;
+  const textContent = `Зовут ${values.name}-${values.male}. Мне ${values.age}, я из ${values.city}. Commented: '${values.commented}'`;
+  const crdivcnt = document.createElement("div");
+  crdivcnt.id = "contentData";
+  titleform.append(crdivcnt);
+  crdivcnt.textContent = textContent;
+  form.reset();
 });
 
 function drawInteractiveCalendar(el) {
@@ -54,10 +21,10 @@ function drawInteractiveCalendar(el) {
                         </table>
                         <div id="content"></div>`;
   el.innerHTML = titleCalendar;
-  let data = new Date();
+  let date = new Date();
   drawCalendar(
-    data.getFullYear(),
-    data.getMonth(),
+    date.getFullYear(),
+    date.getMonth(),
     document.querySelector("#content")
   );
   const titleclick = document.querySelector("#title");
@@ -65,34 +32,32 @@ function drawInteractiveCalendar(el) {
   const spmonth = document.querySelector("#month");
   const spyear = document.querySelector("#year");
 
-  spmonth.innerHTML = data.getMonth() + 1;
-  spyear.innerHTML = data.getFullYear();
+  spmonth.textContent = date.getMonth() + 1;
+  spyear.textContent = date.getFullYear();
 
   titleclick.addEventListener("click", function (e) {
     if (e.target.matches("#last")) {
-      data.setMonth(data.getMonth() - 1);
+      date.setMonth(date.getMonth() - 1);
     } else if (e.target.matches("#next")) {
-      data.setMonth(data.getMonth() + 1);
+      date.setMonth(date.getMonth() + 1);
     }
-    spmonth.innerHTML = data.getMonth() + 1;
-    spyear.innerHTML = data.getFullYear();
+    spmonth.textContent = date.getMonth() + 1;
+    spyear.textContent = date.getFullYear();
     drawCalendar(
-      data.getFullYear(),
-      data.getMonth(),
+      date.getFullYear(),
+      date.getMonth(),
       document.querySelector("#content")
     );
   });
 }
 
 function drawCalendar(year, month, htmlEl) {
-  let data = new Date(year, month, 0);
+  let date = new Date(year, month, 0);
   let dayweek = `<table><tr><td>пн</td><td>вт</td><td>ср</td><td>чт</td><td>пт</td><td>сб</td><td>вс</td></tr><tr>`;
-  for (let i = 0; i < data.getDay(); i++) {
-    dayweek += `<td></td>`;
-  }
+  dayweek += `${"<td></td>".repeat(date.getDay())}`;
   for (
-    let i = data.getDay(), n = 1;
-    i < data.getDate() + data.getDay();
+    let i = date.getDay(), n = 1;
+    i < date.getDate() + date.getDay();
     i++, n++
   ) {
     if (i % 7 === 0 && i !== 0) dayweek += `</tr><tr>`;
