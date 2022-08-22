7e5092375ae2400304977c45a3a9103ddaf61bf6
Your Name <you@example.com>
2022-08-22 21:53:47 +0300

    fix: add prettier

--- 04/ht/calendar/script.js
+++ 04/ht/calendar/script.js
@@ -1,16 +1,16 @@
 // настройки и изображение
 function showCalendarCreate() {
-  const body = document.querySelector('body');
-  const div = document.createElement('div');
+  const body = document.querySelector("body");
+  const div = document.createElement("div");
   body.append(div);
-  
-  div.innerHTML = `<a id="link-calendar" href="">Calendar</a> <a id="link-create" href="">Create</a>`
-  const linkCalendar = document.querySelector('#link-calendar');
-  const linkCreate = document.querySelector('#link-create');
-  linkCalendar.addEventListener('click', function(e) {
+
+  div.innerHTML = `<a id="link-calendar" href="">Calendar</a> | <a id="link-create" href="">Create</a>`;
+  const linkCalendar = document.querySelector("#link-calendar");
+  const linkCreate = document.querySelector("#link-create");
+  linkCalendar.addEventListener("click", function (e) {
     e.preventDefault();
   });
-  linkCreate.addEventListener('click', function(e) {
+  linkCreate.addEventListener("click", function (e) {
     e.preventDefault();
     div.innerHTML += `<div>
     <h4>Configure Calendar</h4>
@@ -27,17 +27,8 @@ function showCalendarCreate() {
     <label for="id">??:</label>
     <input type="number" id="id" name="name" min="1" max="10" required/><br />
     </form>
-    </div>`
+    </div>`;
   });
-
 }
 
 showCalendarCreate();
-
-function linkCalendar() {
-
-}
-
-function linkCreate() {
-  
-}
