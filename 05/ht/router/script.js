1a0e0cde44a8dad0aa1011dc0a3e8bf9fc975980
DimaRK1997 <kedik.grsvdima1997@yandex.by>
2022-09-15 15:33:44 +0300

    docs: update Router add async/await

--- 05/ht/router/script.js
+++ 05/ht/router/script.js
@@ -2,74 +2,71 @@ const routes = [
   {
     name: "index",
     match: "",
-    onBeforeEnter: () => getResultText("onBeforeEnter index"),
-    onEnter: () => getResultText("onEnter index"),
-    onLeave: () => getResultText("onLeave index"),
+    onBeforeEnter: async () => getResultText("onBeforeEnter index"),
+    onEnter: async () => getResultText("onEnter index"),
+    onLeave: async () => getResultText("onLeave index"),
+    isDefault: true,
   },
   {
     name: "city",
     match: /city=(.+)/,
-    onBeforeEnter: (city) => getResultText(`onBeforeEnter city: ${city}`),
-    onEnter: (city) => getResultText(`onEnter city: ${city}`),
-    onLeave: (city) => getResultText(`onLeave city: ${city}`),
+    onBeforeEnter: async (city) => getResultText(`onBeforeEnter city: ${city}`),
+    onEnter: async (city) => getResultText(`onEnter city: ${city}`),
+    onLeave: async (city) => getResultText(`onLeave city: ${city}`),
   },
   {
     name: "about",
     match: (text) => text === "about",
-    onBeforeEnter: () => getResultText(`onBeforeEnter about`),
-    onEnter: () => getResultText(`onEnter about`),
-    onLeave: () => getResultText(`onLeave about`),
+    onBeforeEnter: async () => getResultText(`onBeforeEnter about`),
+    onEnter: async () => getResultText(`onEnter about`),
+    onLeave: async () => getResultText(`onLeave about`),
   },
 ];
 
 function getResultText(text) {
-  document.querySelector(".content").innerHTML += text + "<br>";
+  return (document.querySelector(".content").innerHTML += text + "<br>");
 }
 
 class Router {
   constructor(routes) {
     this.routes = routes;
-    this.currentRoute = null;
-    this.currentParam = "";
     window.addEventListener("hashchange", () => {
       this.handleRouteChange(location.hash.substring(1));
     });
     this.handleRouteChange(location.hash.substring(1));
   }
 
-  handleRouteChange(hash) {
-    const nextRoute = this.selectNextRoute(hash);
+  handleRouteChange = async (hash) => {
+    let currentRoute;
+    try {
+      currentRoute = await this.selectCurrentRoute(hash);
+    } catch (error) {
+      console.log("Ошибка получения роутера " + error);
+    }
+    await (this.lastRoute && this.lastRoute.onLeave && this.lastRoute.onLeave(this.lastParam));
+    this.lastRoute = currentRoute;
+    this.lastParam = this.currentParam;
+    await currentRoute.onBeforeEnter(this.currentParam);
+    await currentRoute.onEnter(this.currentParam);
+  };
 
-    Promise.resolve()
-      .then(() => {
-        this.currentRoute && this.currentRoute.onLeave && this.currentRoute.onLeave(...this.nextParam);
-        this.currentRoute = nextRoute;
-        this.nextParam = this.currentParam;
-      })
-      .then(() => {
-        nextRoute.onBeforeEnter(...this.currentParam);
-      })
-      .then(() => {
-        nextRoute.onEnter(...this.currentParam);
-      });
-  }
+  selectCurrentRoute = async (hash) => {
+    const route =
+      this.routes.find((el) => {
+        const matcher = el.match;
+        if (typeof matcher === "string" && matcher === hash) {
+          return el;
+        }
+        if (matcher instanceof RegExp && hash.match(matcher)) {
+          this.currentParam = hash.match(matcher)[1] || "";
+          return el;
+        }
+        if (typeof matcher === "function" && el.match(hash)) {
+          return el;
+        }
+      }) || this.routes.find((route) => (route.isDefault ? route : ""));
 
-  selectNextRoute(hash) {
-    const route = this.routes.find((el) => {
-      if (typeof el.match === "string" && el.match === hash) {
-        return el;
-      }
-      if (el.match instanceof RegExp && hash.match(el.match)) {
-        Promise.resolve().then(() => {
-          this.currentParam = hash.match(el.match).splice(1, 2) || "";
-        });
-        return el;
-      }
-      if (typeof el.match === "function" && el.match(hash)) {
-        return el;
-      }
-    });
     return route;
-  }
+  };
 }
 const router = new Router(routes);
